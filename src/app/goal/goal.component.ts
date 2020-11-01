5d44479fbe36866e2949f71caa8a261d1f5ab0e1
Bonte Emma BANA <bbonteemma@gmail.com>
2020-11-02 01:49:46 +0200

    changing quotes

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -39,12 +39,12 @@ export class GoalComponent implements OnInit {
     this.goals.push(goal)
   }
   goal: Goal[] = [
-    new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2019,9,14)),
-    new Goal(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2019,6,9)),
-    new Goal(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2019,1,12)),
-    new Goal(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2019,11,18)),
-    new Goal(5,'Solve math homework','Damn Math',new Date(2019,2,14)),
-    new Goal(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2019,3,14)),
+    new Goal(1, 'Love For All, Hatred For None.', 'Khalifatul Masih III',new Date(2019,9,14)),
+    new Goal(2,'Change the world by being yourself. ',' Amy Poehler',new Date(2019,6,9)),
+    new Goal(3,'Every moment is a fresh beginning. ','T.S Eliot',new Date(2019,1,12)),
+    new Goal(4,'Never regret anything that made you smile. ','Mark Twain',new Date(2019,11,18)),
+    new Goal(5,'Everything you can imagine is real.',' Pablo Picasso',new Date(2019,2,14)),
+    new Goal(6,'Whatever you do, do it well.','Walt Disney',new Date(2019,3,14)),
   ];
 
   constructor() { }
